25b0fd6dfe806102945ca7c5d36646dbbfb519b5
Sergey Svyazhin <s.svyazhin@gmail.com>
2017-12-12 10:08:01 +0300

    HomeWork6

--- HomeWork6/HomeWork6.java
+++ HomeWork6/HomeWork6.java
@@ -1,7 +1,67 @@
+/**
+* @author Sergey Svyazhin
+* @version 12.12.2017
+* @link null
+*/
+
 class HomeWork6 {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
-    }
+    	Dog dg = new Dog();
+    	Cat ct = new Cat();
+    	
+    	dg.Run(400);
+    	dg.Swim(9);
+    	dg.Jump(1.5);
+    	
+    	ct.Run(201);
+    	ct.Swim(2);
+    	ct.Jump(2);
 
+    }
+    
+}
+
+abstract class Animal {
+
+	protected double RunDistance;
+	protected double SwimDistance;
+	protected double JumpHeight;
+	
+	public void Run(double Distance) {
+		System.out.println((RunDistance > Distance) ? "Run: true" : "Run: false"); 
+	}
+	
+	public void Swim(double Distance){
+		System.out.println((RunDistance > Distance) ? "Swim: true" : "Swim: false"); 
+	}
+	
+	public void Jump(double Height){
+		System.out.println((JumpHeight > Height) ? "Jump: true" : "Jump: false"); 
+	}
+
+}
+
+class Dog extends Animal {
+
+	Dog() {
+		RunDistance = 500;
+		SwimDistance = 10;
+		JumpHeight = 0.5;
+	}
+	
+}
+
+class Cat extends Animal {
+
+	Cat() {
+		RunDistance = 200;
+		JumpHeight = 2;
+	}
+	
+	@Override
+	public void Swim(double Distance){
+		System.out.println("Swim: false"); 
+	}
+	
 }
